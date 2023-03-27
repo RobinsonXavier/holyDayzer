77be4fb165d78a44223c73ac97b5f0d93cbd1ce6
RobinsonXavier <robinson.webzen@gmail.com>
2023-03-27 13:38:08 -0300

    added data class

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Digite uma data (xx/xx/xxxx): \n");
+        String data = scan.nextLine();
     }
 }
---
+++ src/Data.java
@@ -0,0 +1,16 @@
+public class Data {
+  private String diaDoAno;
+
+  public String getDiaDoAno() {
+    return diaDoAno;
+  }
+
+  public void setDiaDoAno(String diaDoAno) {
+    this.diaDoAno = diaDoAno;
+  }
+  
+  public Data(String diaDoAno) {
+    this.diaDoAno = diaDoAno;
+  }
+  
+}
